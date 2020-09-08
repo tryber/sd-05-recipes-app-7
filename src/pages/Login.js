e91609f664e3ae9eeb3f9282e4aa8784306a69c3
César Alves <alvescesar@hotmail.com>
2020-09-08 20:35:21 -0300

    Login page all requirements done + refactoring

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,28 +2,28 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
-  const [emailInput, setEmailInput] = useState('');
-  const [passwordInput, setpasswordInput] = useState('');
-  const [loginButtonActive, setLoginButtonActive] = useState(true);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginButton, setLoginButton] = useState(true);
 
   // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
-  const validateUser = (email, password) => {
-    const validationRegex = /^(([^<>(\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@(([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    const emailValidated = validationRegex.test(String(email).toLocaleLowerCase());
-    const passwordValidated = password.length > 6;
-    const condition = !(emailValidated && passwordValidated);
-    setLoginButtonActive(condition);
+  const validateUser = (userEmail, userPassword) => {
+    const RegEx = /^(([^<>(\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@(([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const validEmail = RegEx.test(String(userEmail).toLocaleLowerCase());
+    const validPassword = userPassword.length > 6;
+    const condition = !(validEmail && validPassword);
+    setLoginButton(condition);
   };
+
+  useEffect(() => {
+    validateUser(email, password);
+  }, [email, password]);
 
   const setLocalStorage = () => {
     localStorage.setItem('mealsToken', 1);
     localStorage.setItem('cocktailsToken', 1);
-    localStorage.setItem('user', JSON.stringify({ email: emailInput }));
+    localStorage.setItem('user', JSON.stringify({ email }));
   };
-
-  useEffect(() => {
-    validateUser(emailInput, passwordInput);
-  }, [emailInput, passwordInput]);
 
   return (
     <form>
@@ -31,20 +31,20 @@ const Login = () => {
         type="email"
         placeholder="Email"
         data-testid="email-input"
-        onChange={(event) => setEmailInput(event.target.value)}
+        onChange={(event) => setEmail(event.target.value)}
       />
       <input
         type="password"
         placeholder="Senha"
         data-testid="password-input"
-        onChange={(event) => setpasswordInput(event.target.value)}
+        onChange={(event) => setPassword(event.target.value)}
       />
       <Link to="/comidas">
         <button
           type="button"
           data-testid="login-submit-btn"
-          disabled={loginButtonActive}
-          onClick={setLocalStorage()}
+          onClick={() => setLocalStorage()}
+          disabled={loginButton}
         >
           Entrar
         </button>
