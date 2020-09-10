631dc3560a7c497df097688c09b3eab066a946a6
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-09-10 17:31:41 -0300

    Dinks page done

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Footer from '../components/Footer';
 import PageTitle from '../components/Header/PageTitle';
+import DrinkCard from '../components/Cards.js/DrinkCard';
 
 function Drinks() {
   return (
     <div>
       <PageTitle title={'Bebidas'} />
+      <DrinkCard />
       <Footer />
     </div>
   );
--- src/services/fetchDrinks.js
+++ src/services/fetchDrinks.js
@@ -1,1 +1,17 @@
 // Fetch API de bebidas
+function fetchDrinks(text = '', type = '') {
+  let api = '';
+  if (type === 'ingredient') {
+    api = `https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${text}`;
+  } else if (type === 'firstLetter') {
+    api = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${text}`;
+  } else {
+    api = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${text}`;
+  }
+  return fetch(api)
+    .then((response) => response.json())
+    .then((data) => data.drinks)
+    .catch((error) => console.log(error));
+}
+
+export default fetchDrinks;
